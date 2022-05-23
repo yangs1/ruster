ccd344675cc0674f65df5ee2f4b67ae3dedc9142
Yang <ten_year.@Ten-yeardeMacBook-Air.local>
2022-05-24 00:53:22 +0800

    括号检查

--- stack/src/main.rs
+++ stack/src/main.rs
@@ -1,79 +1,63 @@
-
-#[derive(Debug)]
-struct Stack<T>{
-    size: usize,
-    data: Vec<T>
-}
-
-// 简易的栈实现
-impl<T> Stack<T>{
-    fn new() -> Stack<T>{
-        Stack { size: 0, data: vec!() }
-    }
-
-    fn push(&mut self, element: T)
-    {
-        self.push(element);
-        self.size += 1;
-    }
-
-    fn pop(&mut self) -> Option<T>
-    {
-        if self.size == 0 { return None; }
-
-        self.size -= 1;
-
-        self.data.pop()
-    }
-
-    fn peek(&self) -> Option<&T>{
-        if self.size == 0 {
-             return None;
-        }
-        
-        self.data.get(self.size - 1)
-    }
-
-    fn is_empty(&self) -> bool{
-        self.size == 0
-    }
-
-    // 当使用 &mut self 时,
-    // println!("pop {:?}, size {}",s.peek().unwrap(), s.size());  这行会报错
-    // QA: https://course.rs/basic/ownership/borrowing.html ： 同一时刻，你只能拥有要么一个可变引用, 要么任意多个不可变引用
-    fn size(&mut self)-> usize{
-        self.size
-    }
-
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test] 
-    fn stackPushTest(){
-        let mut s = Stack::new();
-        s.push(3);
-
-        assert_eq!(&3, s.peek().unwrap());
-        assert_eq!(3, s.pop().unwrap());
-    }
-}
-
+use stack::stack;
 
 fn main() {
-    let mut s = Stack::new();
+    let mut s = stack::stack::Stack::new();
     s.push(1); 
     s.push(2);
     s.push(4);
 
     // 使用 unwrap 取出 option 的 some() 值 data ,  为 none 抛错
+    // println!("pop {:?}, size {}",s.peek().unwrap(), s.size()); 
 
-    println!("pop {:?}, size {}",s.peek().unwrap(), s.size()); 
     println!("top {:?}",s.peek().unwrap());
     println!(" size {}", s.size());
 
     println!("pop {:?}, size {}",s.pop().unwrap(), s.size());
     println!("is_empty:{}, stack:{:?}", s.is_empty(), s);
+
+    let sa = "(2+3){func}[abc]";
+     let sb = "(2+3)*(3-1";
+     let res1 = par_checker(sa);
+     let res2 = par_checker(sb);
+     println!("sa balanced:{res1}, sb balanced:{res2}");
 }
+
+/* ==================括号算法=============================================== */
+
+pub fn par_checker(par: &str) -> bool {
+    // 解析字符串
+    let mut chars:Vec<char> = Vec::new();
+
+    for c in par.chars() { 
+        chars.push(c);
+    }
+
+    let mut balance = true;
+    let mut stack = stack::stack::Stack::new();
+  
+    let mut index = 0;
+    while index < chars.len() && balance { 
+        let c = chars[index];
+
+        // 3种情况：左括号、右括号、非括号
+        if '(' == c || '{' == c || '[' == c {
+            stack.push(c);
+        } else if ')' == c || ']' == c || '}' == c {
+            if stack.is_empty() {
+                balance = false
+            } else {
+                let cc = stack.pop().unwrap();
+                balance = par_match(cc, c)
+            }
+        }
+        index += 1;
+    }
+
+    balance && stack.is_empty()
+}
+
+fn par_match(first:char, second:char) -> bool{
+    "({[".find(first) == ")}]".find(second)
+}
+
+/* ================================================================= */
---
+++ stack/src/stack.rs
@@ -0,0 +1,65 @@
+// 简易栈 mod
+pub mod stack{
+    #[derive(Debug)]
+    pub struct Stack<T>{
+        size: usize,
+        data: Vec<T>
+    }
+
+    // 简易的栈实现
+    impl<T> Stack<T>{
+        pub fn new() -> Stack<T>{
+            Stack { size: 0, data: vec!() }
+        }
+
+        pub fn push(&mut self, element: T)
+        {
+            self.data.push(element);
+            self.size += 1;
+        }
+
+        pub fn pop(&mut self) -> Option<T>
+        {
+            if self.size == 0 { return None; }
+
+            self.size -= 1;
+
+            self.data.pop()
+        }
+
+        pub fn peek(&self) -> Option<&T>{
+            if self.size == 0 {
+                return None;
+            }
+            
+            self.data.get(self.size - 1)
+        }
+
+        pub fn is_empty(&self) -> bool{
+            self.size == 0
+        }
+
+        // 当使用 &mut self 时,
+        // println!("pop {:?}, size {}",s.peek().unwrap(), s.size());  这行会报错
+        // QA: https://course.rs/basic/ownership/borrowing.html ： 同一时刻，你只能拥有要么一个可变引用, 要么任意多个不可变引用
+        pub fn size(&self)-> usize{
+            self.size
+        }
+
+    }
+
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+
+        #[test] 
+        fn stack_push_test(){
+            let mut s = Stack::new();
+            s.push(3);
+
+            assert_eq!(&3, s.peek().unwrap());
+            assert_eq!(3, s.pop().unwrap());
+        }
+    }
+}
