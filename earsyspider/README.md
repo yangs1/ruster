04795d8c4ef7bc64efef081b6d6d1e93828fb97c
Young <miss_ysp@163.com>
2022-06-21 11:37:35 +0800

    easy_spider

---
+++ earsyspider/README.md
@@ -0,0 +1,3 @@
+#### Description
+写个简易爬虫.....
+可惜 mac 下会出现，  `connection closed via error` 的错误，寻找解决方案中
---
+++ earsyspider/src/main.rs
@@ -0,0 +1,18 @@
+use std::fmt;
+
+
+fn main() {
+    let response = reqwest::blocking::get("https://www.imdb.com/search/title/?groups=top_100&sort=user_rating,desc&count=100")
+    .unwrap()
+    .text()
+    .unwrap();
+    // let content = reqwest::blocking::get("http://httpbin.org/range/26")?.text()?;
+    
+    let document = scraper::Html::parse_document(&response);
+
+    let title_selector = scraper::Selector::parse("h3.lister-item-header>a").unwrap();
+
+    let titles = document.select(&title_selector).map(|x| x.inner_html());
+
+    titles.zip(1..101).for_each(|(item, number)| println!("{}. {}", number, item));
+}
