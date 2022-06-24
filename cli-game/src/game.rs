e1cf9bf2710533176d21a847c1c9bf83dc0706f0
Young <miss_ysp@163.com>
2022-06-24 19:34:35 +0800

    添加 crossterm demo

--- cli-game/src/game.rs
+++ cli-game/src/game.rs
@@ -36,6 +36,67 @@ impl Universe {
     fn get_index(&self, row: usize, column: usize) -> usize {
         (row * self.width + column) as usize
     }
+
+    /**
+     * 翻转，不优雅 有空改改
+     * 
+     */
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                let cell = self.cells[idx];
+                let live_neighbours = self.live_neighbour_count(row, col);
+                next[idx] = match (cell, live_neighbours) {
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    (Cell::Dead, 3) => Cell::Alive,
+                    (otherwise, _) => otherwise,
+                };
+            }
+        }
+        self.cells = next;
+    }
+
+    /**
+     * 去对应的行
+     */
+    fn live_neighbour_count(&self, row: usize, column: usize) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+    
+                let neighbour_row = (row + delta_row) % self.height;
+                let neighbour_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighbour_row, neighbour_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+    
+        count
+    }
+
+    pub fn row_as_string(&self, row: usize) -> Option<String> {
+        if row < self.height {
+            let mut row_string = String::new();
+            let start = self.get_index(row, 0);
+            let end = self.get_index(row, self.width);
+            let line = &self.cells[start..end];
+            for &cell in line {
+                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
+                row_string.push(symbol);
+            }
+            Some(row_string)
+        } else {
+            None
+        }
+    }
+    
     
 }
 
--- cli-game/src/main.rs
+++ cli-game/src/main.rs
@@ -1,9 +1,82 @@
-
 mod game;
 
-fn main() {
-    let mut game = game::Universe::new(5, 5);
+use crossterm::{
+    cursor::{Hide, MoveTo, Show},
+    event::{poll, read, Event, KeyEvent, KeyCode},
+    execute,queue,
+    style::{Color, Print, ResetColor, SetForegroundColor},
+    terminal::{Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen},
+    Result,
+};
+use std::{io::{stdout, Write}, thread::sleep};
+use std::time::Duration;
+use std::{thread, time};
+
+fn  main () {
+	let mut stdout = stdout();
+
+
+	execute!(
+        stdout,
+        EnterAlternateScreen,
+        SetForegroundColor(Color::Magenta),
+		Hide
+
+    ).unwrap();
+}
+/*
+fn main() -> Result<()> {
+
+	let mut game = game::Universe::new(5, 5);
     game.set_cells(&[(2, 1), (2, 2), (2, 3)]);
     print!("{}", game);
+
+	game.tick();
+	let line = game.row_as_string(2);
+	println!("{:?}", line);
+
+	let mut stdout = stdout();
+
+    let mut game = game::Universe::new(5, 5);
+    game.set_cells(&[(2, 1), (2, 2), (2, 3)]);
+    execute!(
+        stdout,
+        EnterAlternateScreen,
+        SetForegroundColor(Color::Magenta),
+        Hide
+    )?;
+
+    loop {
+		if poll(Duration::from_millis(500))? {
+			if let Event::Key(KeyEvent { code, .. }) = read()? {
+				match code {
+					KeyCode::Esc => {
+						break;
+					}
+					_ => {}
+				}
+			}
+		} else {
+			queue!(stdout, Clear(ClearType::All))?;
+			let mut i = 0;
+			while let Some(line) = game.row_as_string(i) {
+				queue!(stdout, MoveTo(0, i as u16), Print(line))?;
+				i += 1;
+			}
+	
+			queue!(
+				stdout,
+				MoveTo(0, (i + 1) as u16),
+				Print("Press Esc to exit...")
+			)?;
+			stdout.flush()?;
+			game.tick();
+		}
+	}
+    execute!(stdout, ResetColor, Show, LeaveAlternateScreen)?;
+
+
+    Ok(())
 }
 
+*/
