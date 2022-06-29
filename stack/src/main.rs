15d9b43e0d4a3ceda00b7e814244182309326432
Young <miss_ysp@163.com>
2022-06-29 15:41:00 +0800

    格式化数据

--- stack/src/main.rs
+++ stack/src/main.rs
@@ -2,41 +2,41 @@ use stack::stack;
 
 fn main() {
     let mut s = stack::stack::Stack::new();
-    s.push(1); 
+    s.push(1);
     s.push(2);
     s.push(4);
 
     // 使用 unwrap 取出 option 的 some() 值 data ,  为 none 抛错
-    // println!("pop {:?}, size {}",s.peek().unwrap(), s.size()); 
+    // println!("pop {:?}, size {}",s.peek().unwrap(), s.size());
 
-    println!("top {:?}",s.peek().unwrap());
+    println!("top {:?}", s.peek().unwrap());
     println!(" size {}", s.size());
 
-    println!("pop {:?}, size {}",s.pop().unwrap(), s.size());
+    println!("pop {:?}, size {}", s.pop().unwrap(), s.size());
     println!("is_empty:{}, stack:{:?}", s.is_empty(), s);
 
     let sa = "(2+3){func}[abc]";
-     let sb = "(2+3)*(3-1";
-     let res1 = par_checker(sa);
-     let res2 = par_checker(sb);
-     println!("sa balanced:{res1}, sb balanced:{res2}");
+    let sb = "(2+3)*(3-1";
+    let res1 = par_checker(sa);
+    let res2 = par_checker(sb);
+    println!("sa balanced:{res1}, sb balanced:{res2}");
 }
 
 /* ==================括号算法=============================================== */
 
 pub fn par_checker(par: &str) -> bool {
     // 解析字符串
-    let mut chars:Vec<char> = Vec::new();
+    let mut chars: Vec<char> = Vec::new();
 
-    for c in par.chars() { 
+    for c in par.chars() {
         chars.push(c);
     }
 
     let mut balance = true;
     let mut stack = stack::stack::Stack::new();
-  
+
     let mut index = 0;
-    while index < chars.len() && balance { 
+    while index < chars.len() && balance {
         let c = chars[index];
 
         // 3种情况：左括号、右括号、非括号
@@ -56,8 +56,28 @@ pub fn par_checker(par: &str) -> bool {
     balance && stack.is_empty()
 }
 
-fn par_match(first:char, second:char) -> bool{
+fn par_match(first: char, second: char) -> bool {
     "({[".find(first) == ")}]".find(second)
 }
 
 /* ================================================================= */
+fn base_converter(mut dec_num: u32, base: u32) -> String {
+    // digits 对应各种余数的字符形式 ， 尤其是 10 - 15
+    let digits = [
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+    ];
+    let mut rem_stack = stack::stack::Stack::new();
+    // 余数入栈
+    while dec_num > 0 {
+        let rem = dec_num % base;
+        rem_stack.push(rem);
+        dec_num /= base;
+    }
+    // 余数出栈并取对应字符来拼接成字符串
+    let mut base_str = "".to_string();
+    while !rem_stack.is_empty() {
+        let rem = rem_stack.pop().unwrap() as usize;
+        base_str += &digits[rem].to_string();
+    }
+    base_str
+}
--- stack/src/stack.rs
+++ stack/src/stack.rs
@@ -1,60 +1,61 @@
 // 简易栈 mod
-pub mod stack{
+pub mod stack {
     #[derive(Debug)]
-    pub struct Stack<T>{
+    pub struct Stack<T> {
         size: usize,
-        data: Vec<T>
+        data: Vec<T>,
     }
 
     // 简易的栈实现
-    impl<T> Stack<T>{
-        pub fn new() -> Stack<T>{
-            Stack { size: 0, data: vec!() }
+    impl<T> Stack<T> {
+        pub fn new() -> Stack<T> {
+            Stack {
+                size: 0,
+                data: vec![],
+            }
         }
 
-        pub fn push(&mut self, element: T)
-        {
+        pub fn push(&mut self, element: T) {
             self.data.push(element);
             self.size += 1;
         }
 
-        pub fn pop(&mut self) -> Option<T>
-        {
-            if self.size == 0 { return None; }
+        pub fn pop(&mut self) -> Option<T> {
+            if self.size == 0 {
+                return None;
+            }
 
             self.size -= 1;
 
             self.data.pop()
         }
 
-        pub fn peek(&self) -> Option<&T>{
+        pub fn peek(&self) -> Option<&T> {
             if self.size == 0 {
                 return None;
             }
-            
+
             self.data.get(self.size - 1)
         }
 
-        pub fn is_empty(&self) -> bool{
+        pub fn is_empty(&self) -> bool {
             self.size == 0
         }
 
         // 当使用 &mut self 时,
         // println!("pop {:?}, size {}",s.peek().unwrap(), s.size());  这行会报错
         // QA: https://course.rs/basic/ownership/borrowing.html ： 同一时刻，你只能拥有要么一个可变引用, 要么任意多个不可变引用
-        pub fn size(&self)-> usize{
+        pub fn size(&self) -> usize {
             self.size
         }
-
     }
-
 
     #[cfg(test)]
     mod test {
         use super::*;
 
-        #[test] 
-        fn stack_push_test(){
+        #[test]
+        fn stack_push_test() {
             let mut s = Stack::new();
             s.push(3);
 
